512bb00054a84522abf73a7cdf4cbc2bd139ca3c
lornah <mukirikabuli@gmail.com>
2022-08-02 13:55:57 +0300

    fourth commit

--- lists.py
+++ lists.py
@@ -1,7 +1,10 @@
 #list example
+from itertools import count
+
+
 suits = ['Spades','Clubs','Diamonds','Hearts']
 
-values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King']
+values = ['Ace', 2, 3, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King']
 
 print (suits[0]) #here,since the lists starts at Index 0, it prints "Spade"
 
@@ -12,3 +15,7 @@ print (suits)
 
 suits.reverse() #reverse allows the list to be sorted in reverse alphabetical order.
 print(suits) 
+
+values.reverse()
+print(values)
+
