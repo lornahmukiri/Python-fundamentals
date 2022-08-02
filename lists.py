c9803b2d350c512e673068270565339ad52e6a83
lornah <mukirikabuli@gmail.com>
2022-08-02 13:22:23 +0300

    first list

--- lists.py
+++ lists.py
@@ -1,3 +1,8 @@
 #list example
-#testing__
-#testing3
+suits = ['Spades','Clubs','Diamonds','Hearts']
+
+values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King']
+
+print (suits[0]) #here,since the lists starts at Index 0, it prints "Spade"
+
+print (values[12]) 
