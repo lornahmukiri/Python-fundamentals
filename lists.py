35e85397572e73928355ae1c2863a5b466c56164
lornah <mukirikabuli@gmail.com>
2022-08-02 13:44:18 +0300

    third commit

--- lists.py
+++ lists.py
@@ -6,3 +6,9 @@ values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King']
 print (suits[0]) #here,since the lists starts at Index 0, it prints "Spade"
 
 print (values[12]) #same here, the index starts at zero so the 12th value to be printed is "King"
+
+suits.sort() #sort() arranges in an alphabetical order.
+print (suits)
+
+suits.reverse() #reverse allows the list to be sorted in reverse alphabetical order.
+print(suits) 
