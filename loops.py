699cb89a97e28f69b5364d43b41904f764d88632
lornah <mukirikabuli@gmail.com>
2022-08-04 13:44:57 +0300

    2nd-commit- VPs

--- loops.py
+++ loops.py
@@ -3,3 +3,9 @@
 #forLoop
 for name in ["Carter", "Reagan", "Obama", "Trump"]:
     print (name + " was a U.S. President.")
+
+for name in ["Mike Pence", "Joe Biden"]:
+    print (name + " was a Vice-President in the USA.")
+
+for name in ["Mwai Kibaki", "Michael Kijana Wamalwa"]:
+    print (name + " was a Vice-President in Kenya")
