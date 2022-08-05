360f6b8630e0a28e8e5191561242678be9d72b8a
lornah <mukirikabuli@gmail.com>
2022-08-05 18:15:42 +0300

    example 2

--- plus_equals.py
+++ plus_equals.py
@@ -10,3 +10,18 @@ milesWalked = 12 #This is our first variable representing the number of miles wa
 milesWalked +=3
 print(milesWalked)
 #...This will print 15.
+#So, instead of recalculating from the start, we've kept a grand total and updated it when we walked further.
+
+
+#Example 2:
+#The += can also be used for string concatenation:
+
+hikeCaption = "What an amazing time to walk through nature!" # Our first variable
+
+#lets add some hashtags that we almost forgot!:
+hikeCaption += "#sunnyDay"
+hikeCaption += "feelingBlessed"
+print(hikeCaption) #., this will print: What an amazing time to walk through nature!#sunnyDayfeelingBlessed
+#what we've done aboe is created a social media caption for the photograph of nature we took on our hike,
+#  but then update the caption to include important social media tags we almost forgot.
+
