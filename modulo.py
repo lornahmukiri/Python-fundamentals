797830e43679352102f3d15dfc2b6fc993a23363
lornah <mukirikabuli@gmail.com>
2022-08-05 15:27:39 +0300

    Exponents prac

---
+++ modulo.py
@@ -0,0 +1,24 @@
+print(29 % 5)
+# Prints 4 because 29 / 5 is 5 with a remainder of 4
+
+print(32 % 3)
+# Prints 2 because 32 / 3 is 10 with a remainder of 2
+
+print(44 % 2)
+# Modulo by 2 returns 0 for even numbers and 1 for odd numbers
+# The above prints 0
+
+# You’re trying to divide a group into four teams. All of you count off, and you get number 27.
+# Find out your team by computing 27 modulo 4. Save the value to my_team.
+# Print out my_team. What number team are you on?
+my_team = (27%4)
+print(my_team)
+#The above will print 3
+
+
+#Food for thought: what number team are the two people next to you (26 and 28) on? 
+# What are the numbers for all 4 teams? 
+print(26%4)
+print(28%4)
+#The above will print 2 and 0 respectively.
+
