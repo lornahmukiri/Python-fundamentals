d1c1df37bd05a44264f8428839d4627c043d4671
lornah <mukirikabuli@gmail.com>
2022-08-05 15:08:17 +0300

    exponents1

---
+++ exponents.py
@@ -0,0 +1,14 @@
+#Exponents are written using the ** notation.
+
+#write 2 to the 10th power
+print(2**10)
+
+#write 8 squared
+print(8**2)
+
+#write 9 cubed
+print(9**3)
+
+#we can even perform fractional exponents, for example:
+#write 4 to the half power 
+print(4**0.5)
