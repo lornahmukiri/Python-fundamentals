0d8bab39b5a6ea2b835fcb855d6b4cfce1842f2c
lornah <mukirikabuli@gmail.com>
2022-08-05 16:08:32 +0300

    example 2

--- concatenation.py
+++ concatenation.py
@@ -1,3 +1,4 @@
+#Example 1:
 greetingText = "Hey there!"
 questionText = "How are you?"
 fullText = greetingText + questionText
@@ -8,3 +9,21 @@ print(fullText)
 fullText = greetingText + " " + questionText
 print(fullText)
 #>>This will print: Hey there! How are you?
+
+
+#Example 2:
+birthdayString = "I am "
+age = 10
+birthdayString2 = " years old today!"
+#Concatenating an integer with a string is possible IF we turn the integer
+#into a string first. 
+# Using str() we can convert variables that arent strings into strings. For example:
+fullBirthdayString = birthdayString + str(age) + birthdayString2
+print(fullBirthdayString)
+#>>This will print: I am 10 years old today!
+
+#BUT, if we just wanna print an integer, we can pass a variable as an argument
+#to print() regardless of whether it is a string. For instance:
+print(birthdayString, age, birthdayString2)
+#>>This will still print: I am  10  years old today!
+
