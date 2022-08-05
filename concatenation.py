f998ac26f7620ff4fd6a31a106f59f3b5deb3653
lornah <mukirikabuli@gmail.com>
2022-08-05 16:10:11 +0300

    Example 3

--- concatenation.py
+++ concatenation.py
@@ -27,3 +27,15 @@ print(fullBirthdayString)
 print(birthdayString, age, birthdayString2)
 #>>This will still print: I am  10  years old today!
 
+
+#Example 3
+string1 = "The wind, "
+string2 = "which had hitherto carried us along with amazing rapidity, "
+string3 = "sank at sunset to a light breeze; "
+string4 = "the soft air just ruffled the water and "
+string5 = "caused a pleasant motion among the trees as we approached the shore, "
+string6 = "from which it wafted the most delightful scent of flowers and hay."
+
+
+message = string1 + string2 + string3 + string4 + string5 + string6
+print(message)
