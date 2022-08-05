2ff00b1db58e591de39f9d49d4a113b169abaff6
lornah <mukirikabuli@gmail.com>
2022-08-05 18:34:10 +0300

    example 2

--- multi-line strings.py
+++ multi-line strings.py
@@ -1,10 +1,14 @@
 #We write multi-line strings by using three quote marks """
 #Otherwise we'll find ourselves with a SyntaxError.
 
-#Example1
+#Example 1
 leaves_of_grass ="""Poets to come! orators, singers, musicians to come!
 Not to-day is to justify me and answer what I am for,
 But you, a new brood, native, athletic, continental, greater than
   before known,
 Arouse! for you must justify me."""
 
+#Example 2
+to_you = """ Stranger, if you passing meet me and desire to speak to me, why
+  should you not speak to me?
+And why should I not speak to you?"""
