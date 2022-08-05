7bb608cf77873564b25fbb2811d0a8616fd453a7
lornah <mukirikabuli@gmail.com>
2022-08-05 18:10:40 +0300

    example1

---
+++ plus_equals.py
@@ -0,0 +1,12 @@
+#Python offers a shorthand for updating variables. 
+# When you have a number saved in a variable and 
+# want to add to the current value of the variable, 
+# you can use the += (plus-equals) operator
+
+#Example 1:
+milesWalked = 12 #This is our first variable representing the number of miles walked
+
+#Then we want to update the variable above to include another 3 miles we walked,. So:
+milesWalked +=3
+print(milesWalked)
+#...This will print 15.
