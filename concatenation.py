cfb838b8b9adf845f1ce2bcbfc95f2e6852b2e21
lornah <mukirikabuli@gmail.com>
2022-08-05 16:00:45 +0300

    concat1

---
+++ concatenation.py
@@ -0,0 +1,10 @@
+greetingText = "Hey there!"
+questionText = "How are you?"
+fullText = greetingText + questionText
+print(fullText)
+#>>This will print: Hey there!How are you?
+
+#Adding some spacing in-between both, so we'll use + " " +
+fullText = greetingText + " " + questionText
+print(fullText)
+#>>This will print: Hey there! How are you?
