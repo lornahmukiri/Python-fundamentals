73a0827ce8ccf000830305e7d3fa08bea25f1f36
lornah <mukirikabuli@gmail.com>
2022-08-19 16:22:35 +0300

    second commit

--- user_input.py
+++ user_input.py
@@ -2,3 +2,5 @@
 #We use the input(), for example:
 
 favourite_city = input("What is your favourite city?")
+
+print("Oh nice! I like " + favourite_city + "too, but I think my favourite city is Nairobi")
