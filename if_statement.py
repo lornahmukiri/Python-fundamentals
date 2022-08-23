2514cdf05fc35d132a245a73372d09736938a498
lornah <mukirikabuli@gmail.com>
2022-08-23 14:53:54 +0300

    second statement

--- if_statement.py
+++ if_statement.py
@@ -1,4 +1,9 @@
 userName = "Kendi"
 
-if userName = "Davis":
+if userName == "Davis":
     print("Get off my computer Davis!")
+
+userName = "Brenda"
+
+if userName == "Brenda":
+    print("I know it's still you Davis! Go away!")
