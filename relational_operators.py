5544a85513e080e15f76500cee29c0a62234a945
lornah <mukirikabuli@gmail.com>
2022-08-23 15:47:47 +0300

    Second statement

--- relational_operators.py
+++ relational_operators.py
@@ -7,3 +7,14 @@ age = 8
 #the condition
 if age <= 13:
     print("Sorry, parental control is required")
+
+#Example 2
+x = 20
+y = 20
+
+if x == y:
+    print("These numbers are the same")
+
+uniCredits = 120
+
+if uniCredits
