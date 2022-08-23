fb705dd7dcd894e675f313698890512c6f5b2b51
lornah <mukirikabuli@gmail.com>
2022-08-23 14:37:31 +0300

    variables

---
+++ boolean_variable.py
@@ -0,0 +1,9 @@
+my_baby_bool = "true"
+
+type(my_baby_bool) #Checking the type of varible. This is a string due to the quotation marks.
+print(type(my_baby_bool))
+
+my_baby_bool_two = True 
+type(my_baby_bool_two) #Checking the type of variable. This is a boolean.
+#Boolean v
+print(type(my_baby_bool_two))
