e40c3a659d5440f5b443de560c8bea22a0ad1478
lornah <mukirikabuli@gmail.com>
2022-08-23 15:46:24 +0300

    variable definition

--- relational_operators.py
+++ relational_operators.py
@@ -1,5 +1,9 @@
 #Write a condition for a movie streaming platform which 
 #checks if users are over 13. If they aren't, print "Sorry, parental control required"
 
+#Define age variable
+age = 8
+
+#the condition
 if age <= 13:
     print("Sorry, parental control is required")
