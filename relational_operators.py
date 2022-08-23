85d01f04d8a893fdbbe8abdde1dda93acd02e205
lornah <mukirikabuli@gmail.com>
2022-08-23 15:45:36 +0300

    first statement

---
+++ relational_operators.py
@@ -0,0 +1,5 @@
+#Write a condition for a movie streaming platform which 
+#checks if users are over 13. If they aren't, print "Sorry, parental control required"
+
+if age <= 13:
+    print("Sorry, parental control is required")
