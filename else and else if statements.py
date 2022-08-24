9d4305eeddbc5b1cbbe6759f2de8b53bebd2ad2d
lornah <mukirikabuli@gmail.com>
2022-08-24 12:29:18 +0300

    example 3_elif

--- else and else if statements.py
+++ else and else if statements.py
@@ -14,3 +14,12 @@ if (credits >= 120) and (gpa >= 2.0):
     print("Congratulations. You meet the graduation requirements")
 else:
     print("You do not meet the requirements to graduate.")
+
+
+#Example 3:
+donation = 5000
+
+if donation >=1000:
+    print("You've achieved platinum status")
+elif donation >= 500:
+    print("You've achived gold status")
