faa0b01fc6d85788d0b31c39781d499c25206c36
lornah <mukirikabuli@gmail.com>
2022-08-24 11:31:11 +0300

    example 2_if statement

---
+++ else and else if statements.py
@@ -0,0 +1,16 @@
+age = 12
+
+if age >= 13:
+    print("Access granted.")
+else:
+    print("Sorry, you must be 13 or older to watch this movie.")
+
+
+#example 2
+credits = 120
+gpa = 1.9
+
+if (credits >= 120) and (gpa >= 2.0):
+    print("Congratulations. You meet the graduation requirements")
+else:
+    print("You do not meet the requirements to graduate.")
