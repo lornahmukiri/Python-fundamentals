541036793e035f1d5dec09b54ce277f862907030
lornah <mukirikabuli@gmail.com>
2022-08-24 17:30:24 +0300

    drone shipping comments

--- shipping.py
+++ shipping.py
@@ -24,7 +24,13 @@
 
 #Flat charge: $125.00
 
+#Drone Shipping
 
+#Weight of Package	Price per Pound	Flat Charge
+#2 lb or less	$4.50	$0.00
+#Over 2 lb but less than or equal to 6 lb	$9.00	$0.00
+#Over 6 lb but less than or equal to 10 lb	$12.00	$0.00
+#Over 10 lb	$14.25	$0.00
 
 weight = 41.5
 
