75b43eeb2b1fa75b076fb2d6b1499e6a00f7a2ec
lornah <mukirikabuli@gmail.com>
2022-08-24 13:50:20 +0300

    code solution

--- magic8.py
+++ magic8.py
@@ -1,7 +1,7 @@
 #CODE CHALLENGE:
+#The Magic 8-Ball is a popular toy developed in the 1950s for fortune-telling or advice seeking.
 
-
-
+#Write a magic8.py Python program that can answer any “Yes” or “No” question with a different fortune each time it executes.
 
 
 import random
