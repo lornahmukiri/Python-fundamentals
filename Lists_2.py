f779a279268bfdfaaee0af2ff3f1322168944dfd
lornah <mukirikabuli@gmail.com>
2022-08-25 13:34:27 +0300

    first example

---
+++ Lists_2.py
@@ -0,0 +1,7 @@
+#Example 1
+#Create a new list called sam_height_and_testscore that contains:
+
+#The string "Sam" (to represent Sam’s name)
+#The number 67 (to represent Sam’s height)
+#The float 85.5 (to represent Sam’s score)
+#The boolean True (to represent Sam passing the test)
