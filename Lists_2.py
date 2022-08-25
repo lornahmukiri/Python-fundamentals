dfb5b8bcf92e46e50db1f08b769219fcc006b305
lornah <mukirikabuli@gmail.com>
2022-08-25 14:40:52 +0300

    remove example

--- Lists_2.py
+++ Lists_2.py
@@ -9,8 +9,12 @@
 sam_height_and_testscore = ["Sam", 67, 85.5, True]
 
 
-#List methods. 
+#List methods: Examples:
 #.append() adds an element at the end of a list. E.g.
 example_List = [1, 2, 3, 4]
 example_List.append(5)
+print(example_List)
+
+#.remove() removes an element in a list. E.g. 
+example_List.remove(4)
 print(example_List)
