f8840e25033ff51e68fd296c67397d63a1908ff4
lornah <mukirikabuli@gmail.com>
2022-08-25 16:39:35 +0300

    last comment

--- Lists_2.py
+++ Lists_2.py
@@ -50,6 +50,6 @@ garden_waitlist = ["Jiho", "Adam", "Sonny", "Alisha"]
 
 garden_waitlist[1] = "Calla"
 print(garden_waitlist)
-
+#lets replace "Alisha" with "Alex"
 garden_waitlist [-1] = "Alex"
 print(garden_waitlist)
