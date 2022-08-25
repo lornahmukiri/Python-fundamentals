53124b40b69272dfa795fd1b72bc821981203aac
lornah <mukirikabuli@gmail.com>
2022-08-25 16:49:31 +0300

    using remove

--- Lists_2.py
+++ Lists_2.py
@@ -53,3 +53,21 @@ print(garden_waitlist)
 #lets replace "Alisha" with "Alex"
 garden_waitlist [-1] = "Alex"
 print(garden_waitlist)
+
+#Shrinking a list using .remove()
+#Example 1:
+order_list = ["Celery", "Orange Juice", "Orange","Flatbread"]
+print(order_list)
+
+order_list.remove("Flatbread")
+print(order_list)
+
+new_store_order_list = ["Orange", "Apple", "Mango", "Broccoli", "Mango"]
+print(new_store_order_list)
+
+new_store_order_list.remove("Mango")
+print(new_store_order_list)
+
+new_store_order_list.remove("Onions")
+print(new_store_order_list)
+#When we call .remove() on a list with a value that does not exist, we will receive a ValueError, as is the case above...
