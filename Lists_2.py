8c635cddd26159f820f097ad69bfc6db95f48a71
lornah <mukirikabuli@gmail.com>
2022-08-25 13:35:30 +0300

    new list

--- Lists_2.py
+++ Lists_2.py
@@ -5,3 +5,5 @@
 #The number 67 (to represent Sam’s height)
 #The float 85.5 (to represent Sam’s score)
 #The boolean True (to represent Sam passing the test)
+
+sam_height_and_testscore = ["Sam", 67, 85.5, True]
