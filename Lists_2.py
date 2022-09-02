907835278940bfac1ec11bbef57eed22b146ca1f
lornah <mukirikabuli@gmail.com>
2022-09-02 22:41:48 +0300

    fourth commit

--- Lists_2.py
+++ Lists_2.py
@@ -134,3 +134,12 @@ customer_data = [
   ["Depak", "Medium", False]
 ]
 print(customer_data)
+
+customer_data[2][2] = False
+print(customer_data)
+
+customer_data[1].remove(False)
+print(customer_data)
+
+customer_data_final =  customer_data + [["Amit", "Large", True], ["Karim", "X-Large", False]]
+print(customer_data_final)
